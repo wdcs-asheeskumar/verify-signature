1b3d9b9751eb8a297afc001a5da4bd46372f28da
wdcs-asheeskumar <154244737+wdcs-asheeskumar@users.noreply.github.com>
2024-02-14 16:26:32 +0530

    Delete .deps directory

--- .deps/github/OpenZeppelin/openzeppelin-contracts/contracts/interfaces/IERC20.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (interfaces/IERC20.sol)
-
-pragma solidity ^0.6.0;
-
-import {IERC20} from "../token/ERC20/IERC20.sol";
-// .deps/github/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol
